85b9e5f5485aae345d06cb960576f0f437a187d0
Gabriel <gbrcenciati@gmail.com>
2022-08-05 20:15:08 -0300

    add: data pipeline scaffold

---
+++ {{ cookiecutter.repo_name }}/src/pipeline/main_pipeline.py
@@ -0,0 +1,116 @@
+from src.pipeline.stages import data_cleaning
+from src.pipeline.stages import feature_engineering
+from src.pipeline.stages import data_preprocessing
+from src.pipeline.stages import recreate_model
+from src.pipeline.stages import retrain_model
+from src.pipeline.stages import get_prediction
+
+class Pipeline:
+    '''Placeholder.
+
+    Attributes
+    ----------
+    Placeholder.
+
+    Methods
+    -------
+    Placeholder.
+    '''
+    
+    def __init__(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+
+    def data_cleaning(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+
+    def feature_engineering(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+
+    def data_preprocessing(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+    
+    def recreate_model(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+    
+    def retrain_model(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
+
+    def get_prediction(self) -> None:
+        '''Placeholder.
+
+        Params
+        ------
+        param : type, default=placeholder.
+            description
+        
+        Returns
+        -------
+        Placeholder.
+        '''
+        pass
